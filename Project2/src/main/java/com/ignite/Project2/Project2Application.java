7e5551a3d9111977cb54284e5e31ac09c37aba7d
jstezalski <janstezalski@gmail.com>
2020-02-18 12:39:35 +0100

    Added git ignore

--- Project2/src/main/java/com/ignite/Project2/Project2Application.java
+++ Project2/src/main/java/com/ignite/Project2/Project2Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CountingApplication {
+public class Project2Application {
     public static void main(String[] args) {
-        SpringApplication.run(CountingApplication.class, args);
+        SpringApplication.run(Project2Application.class, args);
     }
 }
